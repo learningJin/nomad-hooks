bb39f779ee80c9096796e79c6ba43dceb6b7f2a0
wonjinYi <studioplug17@gmail.com>
2021-07-25 02:16:17 +0900

    useFullScreen

--- src/App.js
+++ src/App.js
@@ -2,33 +2,63 @@
 import './App.css';
 import React, { useEffect, useState, useRef } from 'react';
 
-const useScroll = () => {
+const useFullScreen = (callback) => {
+    const element = useRef();
 
-    const [state, setState] = useState({
-        x: 0,
-        y: 0,
-    });
+    const triggerFull = () => {
+        if (element.current) {
+            if(element.current.requestFullscreen){
+                element.current.requestFullscreen();
+            } else if(element.current.mozRequestFullscreen) {
+                element.current.mozRequestFullscreen();
+            } else if(element.current.webkitRequestFullscreen){
+                element.current.webkitRequestFullscreen();
+            } else if(element.current.msRequestFullscreen){
+                element.current.msRequestFullscreen();
+            }
 
-    const onScroll = event => {
-        setState({
-            x : window.scrollX,
-            y : window.scrollY,
-        });
+            if(callback && typeof callback==='function'){
+                callback(true);
+            }
+        }
     }
-    useEffect(() => {
-        window.addEventListener('scroll', onScroll);
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
+    const exitFull = () => {
+        if(document.exitFullscreen){
+            document.exitFullscreen();
+        } else if(document.mozCancelFullScreen){
+            document.mozCancelFullScreen(); 
+        } else if (document.webkitExitFullscreen){
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen){
+            document.msExitFullscreen();
+        }
 
-    return state;
+        if(callback && typeof callback==='function'){
+            callback(false);
+        }
+    }
+
+    return { 
+        element, 
+        triggerFull, 
+        exitFull 
+    };
 }
 
 const App = () => {
-    const { y } = useScroll();
+    const onFullS = (isFull) => {
+        console.log(isFull);
+    }
+
+    const { element, triggerFull, exitFull } = useFullScreen(onFullS);
 
     return (
         <div className="App" style={{ height: "1000vh" }}>
-            <h1 style={{ position: 'fixed', color: y > 100 ? 'red' : 'blue' }}>hihi</h1>
+            <div ref={element}>
+                <img alt="aa" src="https://www.knou.ac.kr/images/knou/common/logo-header.png" />
+                <button onClick={exitFull}>nomal screen</button>
+            </div>
+            <button onClick={triggerFull}>full screen</button>
         </div>
     );
 }
